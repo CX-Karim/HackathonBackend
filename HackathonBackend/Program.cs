ea56d45313a9e9e05807371a47cf8204f546d12b
karimH <karimH@cxunicorn.com>
2021-09-15 22:38:26 +0400

    Add project files.

---
+++ HackathonBackend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackathonBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
