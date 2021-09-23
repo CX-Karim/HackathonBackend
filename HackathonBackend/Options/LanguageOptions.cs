9b126827324a5bf3979bf03161629ef22751771d
karimH <karimH@cxunicorn.com>
2021-09-23 16:05:38 +0400

    push

---
+++ HackathonBackend/Options/LanguageOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackathonBackend.Options
+{
+    class LanguageOptions
+    {
+        public static readonly string options = "IdentifiableLanguages";
+        public LangConfig[] IdentifiableLanguages;
+    }
+
+    class LangConfig{
+        public string name { get; set; }
+        public string code { get; set; }
+    }
+}
--- HackathonBackend/Program.cs
+++ HackathonBackend/Program.cs
@@ -1,11 +1,39 @@
 ﻿using System;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Translation;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Threading.Tasks;
+
 
 namespace HackathonBackend
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Transcribe() 
         {
+            var config = SpeechTranslationConfig.FromSubscription("TEXT ME TO GET IT", "westeurope");
+            var autoDetectSourceLanguageConfig =
+            AutoDetectSourceLanguageConfig.FromLanguages(
+            new string[] { "ar-EG", "en-US", "fr-FR" });
+
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using (var recognizer = new SpeechRecognizer(
+                config,
+                autoDetectSourceLanguageConfig,
+                audioConfig))
+            {
+                var speechRecognitionResult = await recognizer.RecognizeOnceAsync();
+                var autoDetectSourceLanguageResult =
+                    AutoDetectSourceLanguageResult.FromResult(speechRecognitionResult);
+                var detectedLanguage = autoDetectSourceLanguageResult.Language;
+                config.SpeechRecognitionLanguage = detectedLanguage;
+                config.AddTargetLanguage("ar-EG");
+                Console.WriteLine(detectedLanguage);
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            await Transcribe();
             Console.WriteLine("Hello World!");
         }
     }
